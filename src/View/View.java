292b8775fb0a7ba73090e6243413bb1188e559df
SergeyKostin <sergey.kostin1996@yandex.ru>
2016-11-21 19:15:06 +0400

    Merge origin/master

--- src/View/View.java
+++
@@ -1,7 +0,0 @@
-package View;
-import trackList.TrackList;
-
-public interface View {
-    public void printTrackList(TrackList trackList);
-    public void printTracksByNameOfType(String type,TrackList trackList, String typeName);
-}
--- src/trackList/Repository.java
+++
@@ -1,9 +0,0 @@
-package trackList;
-
-import java.util.ArrayList;
-
-public interface Repository {
-    Track getTrackByName(String name);
-    Track findTrackByName(String name);
-    ArrayList<Track> scanForTracks(String path);
-}
